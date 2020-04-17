e7f830b4e05325e56fabaf6f945945f458301142
Andrea Rizzi <rizziandrea@gmail.com>
2020-04-17 15:19:34 +0200

    adding multi level menu and checkbox

--- js/functions.js
+++ js/functions.js
@@ -6,9 +6,39 @@ function validateForm() {
     }
     return true;
 }
+function updateCampaign() {
+	var sIdx = document.getElementById("selectSite").selectedIndex;
+	var sStr = document.getElementById("selectSite").options[sIdx].value;
+	console.log(sStr);
+	var camp =document.getElementById("selectCampaign");
+	for (s in camp.options) { camp.options.remove(0); }
+	for(var k in optionsMap[sStr])
+	{
+		var opt = document.createElement("option"); 
+		opt.text = k
+		camp.add(opt)
+		console.log(k);
+	}
+ updateTestID();
+}
+
+
 
 function updateTestID() {
-    var y = document.getElementById("selectCampaign").selectedIndex;
-    document.getElementById("selectTestID").innerHTML = TestIDopts[y];
+        var sIdx = document.getElementById("selectSite").selectedIndex;
+        var sStr = document.getElementById("selectSite").options[sIdx].value;
+	var cIdx = document.getElementById("selectCampaign").selectedIndex;
+	var cStr = document.getElementById("selectCampaign").options[cIdx].value;
+	console.log(cStr);
+	var site =document.getElementById("selectTestID");
+	for (s in site.options) { site.options.remove(0); }
+	for(var k in optionsMap[sStr][cStr])
+	{
+		var opt = document.createElement("option"); 
+		opt.text = optionsMap[sStr][cStr][k]
+		site.add(opt)
+		console.log(k);
+	}
 }
+
 
